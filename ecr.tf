9a9798eb1f6afbe5faaf471b7d3970a9a7387106
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-10-18 09:40:18 +0000

    Initial commit, adding githooks submodule

--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "docker-ecr-template" {
-  name = "docker-ecr-template"
+resource "aws_ecr_repository" "docker-ecr-ap-python-image" {
+  name = "docker-ecr-ap-python-image"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/docker-ecr-template" }
+    { DockerHub : "dwpdigital/docker-ecr-ap-python-image" }
   )
 }
 
-resource "aws_ecr_repository_policy" "docker-ecr-template" {
-  repository = aws_ecr_repository.docker-ecr-template.name
+resource "aws_ecr_repository_policy" "docker-ecr-ap-python-image" {
+  repository = aws_ecr_repository.docker-ecr-ap-python-image.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.docker-ecr-template.repository_url
+  value = aws_ecr_repository.docker-ecr-ap-python-image.repository_url
 }
